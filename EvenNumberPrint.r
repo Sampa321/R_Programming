97584bd74e1a01b5e47854911b45fe1dfff4e952
Sampa Nayak <sampanayak987@gmail.com>
2025-08-10 22:15:27 +0530

    Add files via upload
    WAP using while loop that prints all even numbers from 1 to 20.

---
+++ EvenNumberPrint.r
@@ -0,0 +1,18 @@
+#WAP using while loop that prints all even numbers from 1 to 20.
+i<-1
+while(i!=21)
+{
+    if(i%%2==0)
+    {
+        print(i)
+    }
+    i<-i+1
+}
+
+
+i<-2
+while(i!=22)
+{
+    print(i)
+    i<-i+2
+}
