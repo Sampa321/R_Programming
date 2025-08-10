d51b2b3fe1f870b83ef44df56bfba142e3e2037d
Sampa Nayak <sampanayak987@gmail.com>
2025-08-10 22:26:06 +0530

    Add files via upload
    Create a vector of names ( e .g ,"Alice" , "Bob" ,"Charlie").Print the length of vector and the first and last elements.

---
+++ Vector.r
@@ -0,0 +1,7 @@
+#Create a vector of names(e.g.,"Alice","Bob","Charlie").Print the length of vector and the first and last elements.
+vector<-c("Alice","Bob","Charlie")
+print(paste("Length of vector:",length(vector)))
+print(paste("First element of vector:",vector[1]))
+print(paste("Last element of vector:",vector[3]))
+
+
