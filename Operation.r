7a1774fe17411eca130b0bb155755a87f81d75ae
Sampa Nayak <sampanayak987@gmail.com>
2025-08-10 22:18:52 +0530

    Add files via upload
    Create a  variable  a with a value of 10 and b with a value of 5.perform the following operations: addition , subtraction, multiplication ,division, and exponential between a and b.

---
+++ Operation.r
@@ -0,0 +1,18 @@
+#Create a  variable  a with a value of 10 and b with a value of 5.perform the following operations:
+#addition,subtraction,multiplication,division,and exponential between a nad b.
+a<-10
+b<-5
+#addition
+print(a+b)
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#division
+print(a/b)
+
+#exponential
+print(a^b)
