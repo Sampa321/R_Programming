3ebc606ab87a2ccebbef181a12f0890452bfa630
Sampa Nayak <sampanayak987@gmail.com>
2025-08-10 22:14:43 +0530

    Add files via upload
    Write  a script that checks if a number is positive , negative or zero.

---
+++ CheckPositiveNegative.r
@@ -0,0 +1,14 @@
+#qs4:Write  a script that checks if a number is positive,negative or zero.
+number<-as.integer(readline("Enter a number:"))
+if(number>0)
+{
+    print("positive")
+}
+else if(number<0)
+{
+    print("Negative")
+}
+else {
+   print("Zero")
+}
+ 
